9d87e9d80842aec9f5122cb066c2cff892e86be7
Marcelo Oliveira <marcelo@marceloweb.info>
2019-11-11 18:55:09 -0300

    - structure

--- twitter.go
+++ twitter.go
@@ -1,19 +1,26 @@
 package main
 
 import (
-
+  "fmt",
+  "net/http"
 )
 
 const (
-
+   maxPosts = 20
 )
 
-type action func() ()
+type Parameters struct {
+   Name string `json:"name"`
+   User string `json:"user"`
+   Followers int `json:"followers"`
+}
 
-func getData(key) string {
-
+func getData(key string) {
+   apiError := new(APIError)
+   resp, err := s.sling.New().Get()
+   return resp, err
 }
 
 func main() {
-
+  return getData()
 } 
